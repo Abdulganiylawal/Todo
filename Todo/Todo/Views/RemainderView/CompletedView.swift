e3bdbce055bea5957919ff41b7c63265888b67bf
Lawal Abdulganiy <labdulghaneey@gmail.com>
2023-10-01 21:12:56 +0100

    still woking on the remainder view. WHY GOD

---
+++ Todo/Todo/Views/RemainderView/CompletedView.swift
@@ -0,0 +1,18 @@
+//
+//  CompletedView.swift
+//  Todo
+//
+//  Created by Lawal Abdulganiy on 01/10/2023.
+//
+
+import SwiftUI
+
+struct CompletedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompletedView()
+}
