1378235134b0d26002c26a85bb6ec454968285b2
Lawal Abdulganiy <labdulghaneey@gmail.com>
2023-11-12 23:40:52 +0100

    worked on editing remainder logic

---
+++ Todo/Todo/Views/RemainderView/EditRemainder.swift
@@ -0,0 +1,18 @@
+//
+//  EditRemainder.swift
+//  Todo
+//
+//  Created by Lawal Abdulganiy on 12/11/2023.
+//
+
+import SwiftUI
+
+struct EditRemainder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditRemainder()
+}
