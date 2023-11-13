c05b17e1715e7aa52aa8fba17f1e49b8a6b1b93c
Lawal Abdulganiy <labdulghaneey@gmail.com>
2023-11-13 22:07:17 +0100

    Finished With the redesign and wrote some unit test

---
+++ Todo/Todo/Views/ListView/EditList.swift
@@ -0,0 +1,18 @@
+//
+//  EditList.swift
+//  Todo
+//
+//  Created by Lawal Abdulganiy on 13/11/2023.
+//
+
+import SwiftUI
+
+struct EditList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditList()
+}
